d31872e1d9e0b1516dde74399f07aad35b0f8dca
Chris White <chrisw@leehayes.com>
2018-07-05 12:28:49 -0400

    Can now run from a DOS prompt
    The Makefile now copies the relevant Cygwin files into this directory,
    and modifies them all to look for `zygwin1.dll` instead of
    `cygwin1.dll`.  This way we can run from a DOS prompt without an
    existing Cygwin installation, and without stepping on Cygwin if it is
    installed.
    
    runme.bat is the equivalent of runme.sh.  It uses the window
    configuration given in `default.view`.

--- Makefile
+++ Makefile
@@ -11,9 +11,38 @@ OSG=../osg/build-gcc
 LOL=../lua-osg-livecoding
 LOLBUILD=${LOL}/build-gcc
 
+# DLLs we use
+DLLS=	/usr/bin/cygcdt-5.dll \
+	/usr/bin/cygcgraph-6.dll \
+	/usr/bin/cygcrypto-1.0.0.dll \
+	/usr/bin/cygexpat-1.dll \
+	/usr/bin/cyggcc_s-seh-1.dll \
+	/usr/bin/cyggvc-6.dll \
+	/usr/bin/cygltdl-7.dll \
+	/usr/bin/cyglua-5.2.dll \
+	/usr/bin/cygpathplan-4.dll \
+	/usr/bin/cygstdc++-6.dll \
+	/usr/bin/cygz.dll \
+	/usr/bin/cygportaudio-2.dll \
+	/usr/bin/cygsndfile-1.dll \
+	/usr/bin/cygFLAC-8.dll \
+	/usr/bin/cyggsm-1.dll \
+	/usr/bin/cygogg-0.dll \
+	/usr/bin/cygvorbis-0.dll \
+	/usr/bin/cygvorbisenc-2.dll \
+
 # Show the package size after updating.
-all: osg other livecoding.exe
+all: zygwin
 	du -cks --exclude='.git/*' .
+
+# Call ourselves "zygwin" to avoid conflict with an existing cygwin installation
+zygwin: osg other livecoding.exe
+	cp `which cygwin1.dll` bin/zygwin1.dll
+	# ldd dependencies
+	cp ${DLLS} bin
+	#
+	find . \( -name \*.exe -o -name \*.dll -o -name \*.so \) -print0 | \
+		xargs -0 -n1 sed -i 's/cygwin1\.dll/zygwin1.dll/g'
 
 # Copy and strip OSG DLLs
 osg:
@@ -29,6 +58,7 @@ other:
 	### Other files ##################################################
 	cp -R ${LOL}/{assets,legal,lua,samples} .
 
+#
 livecoding.exe: ${LOLBUILD}/livecoding.exe
 	### livecoding.exe ###############################################
 	cp $< $@
---
+++ runme.bat
@@ -0,0 +1,67 @@
+@echo off
+:: runme.bat: Run lua-osg-livecoding
+:: Copyright (c) 2018 cxw/Incline
+::#!/bin/bash
+
+::# Add the local Lua rocks to the Lua paths
+::d="$(pwd)"
+set LUA_CPATH=lua-5.2/?.dll;lua-5.2/?.so;lua-5.2/loadall.dll;?.dll
+set LUA_PATH=lua/?.lua;lua-5.2/?.lua;lua-5.2/?/init.lua;?.lua
+
+::export LUA_CPATH="${new_cpath}${LUA_CPATH:+;${LUA_CPATH}}"
+::export LUA_PATH="${new_lpath}${LUA_PATH:+;${LUA_PATH}}"
+
+::# Add the local binaries to the path
+
+PATH %PATH%;bin;bin/osgPlugins-3.5.6
+::export PATH="${new_path}${PATH:+:${PATH}}"
+
+::# We have to run windowed --- full-screen isn't supported yet for some reason.
+::set OSG_WINDOW="100 100 600 400"
+::   This doesn't take effect for some reason.  Therefore, we use the
+::   -c option below.
+
+::# Debug info
+::cat <<EOF
+echo PATH:
+PATH
+
+echo LUA_PATH:
+echo %LUA_PATH%
+
+echo LUA_CPATH:
+echo %LUA_CPATH%
+
+::EOF
+
+::# Fire it up!
+::case "$1" in
+::    --help) cat <<EOF
+::runme.sh: run lua-osg-livecoding
+::Options (only detected if the first parameter):
+::    --help              Print this help.
+::    -g                  Run livecoding in gdb.
+::    -i, --interactive   Spawn a shell instead of running livecoding.exe.
+::                        You can run livecoding.exe in that shell.
+
+::Any other options are passed to livecoding.exe.
+::EOF
+::            exit
+::            ;;
+::    -i|--interactive)
+::            export PS1="lol $PS1"
+::            exec "$SHELL" -i
+::            exit
+::            ;;
+::esac
+
+::if [[ $1 == -g ]]; then
+::    gdb --args "${d}/livecoding.exe" "$@"
+::else
+::    "${d}/livecoding.exe" "$@"
+::fi
+livecoding -c default.view %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+::stty sane   # just in case of an abort
+
+::# vi: set ts=4 sts=4 sw=4 et ai ff=dos: #
